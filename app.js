a391ba5101a142fd8ddb27e7e92e525efde6607d
Drancer55 <antonio281255@gmail.com>
2021-09-24 09:15:59 -0500

    Upload Version

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 alert ("Saludos marinero, gracias por tu visita, argh!!")
-
+//Variable "i" para el contador final
+var i= 0;
 //var app = "Trivia" (ejemplo de variable)
 
 var userName = prompt ("Queremos saber de ti ¿Cómo te llamas?")
@@ -18,14 +19,15 @@ if (confirm("Argh!! Bienvenido a la tripulación " + userName + " ¿Eres tan rud
 //Alert
         if (question1 == correctAnswer1){
             alert("Correcto!!");
+            i++;
         } else {
             alert("Incorrecto (u.u) ");
         } 
 //Escribir en HTML
         if (question1 == correctAnswer1){
-            cuestionOne = ("<b>Correcto</b>")
+            cuestionOne = ("<b>Correcto</b>");
         }   else {
-            cuestionOne = ("Incorrecto, la respuesta correcta es:\n <b>c) Se busca ayuda (Help Wanted)</b>")
+            cuestionOne = ("Incorrecto, la respuesta correcta es:\n <b>c) Se busca ayuda (Help Wanted)</b>");
         }
         document.getElementById("evaluation1").innerHTML = cuestionOne;
 
@@ -38,14 +40,15 @@ if (confirm("Argh!! Bienvenido a la tripulación " + userName + " ¿Eres tan rud
         
         if (question2 == correctAnswer2){
             alert("Correcto!!\nTenemos un buen tripulante.");
+            i++;
         } else {
             alert("Casi, pero no :c");
         }
 
         if (question2 == correctAnswer2){
-            cuestionTwo = ("<b>Correcto</b>")
+            cuestionTwo = ("<b>Correcto</b>");
         }   else {
-            cuestionTwo = ("Incorrecto, la respuesta correcta es:\n <b>b) Stephen McDannell Hillenburg</b>")
+            cuestionTwo = ("Incorrecto, la respuesta correcta es:\n <b>b) Stephen McDannell Hillenburg</b>");
         }
         document.getElementById("evaluation2").innerHTML = cuestionTwo;
 
@@ -57,55 +60,58 @@ if (confirm("Argh!! Bienvenido a la tripulación " + userName + " ¿Eres tan rud
     var  correctAnswer3 = "c";
 
         if (question3 == correctAnswer3){
-            alert("La esposa computadora!!")
+            alert("La esposa computadora!!");
+            i++;
         } else {
-            alert("No en esta vida, viejo u.u")
+            alert("No en esta vida, viejo u.u");
         }   
 
         if (question3 == correctAnswer3){
-            cuestionThree = ("<b>Correcto</b>")
+            cuestionThree = ("<b>Correcto</b>");
         }   else {
-            cuestionThree = ("Incorrecto, la respuesta correcta es:\n <b>c) Karen</b>")
+            cuestionThree = ("Incorrecto, la respuesta correcta es:\n <b>c) Karen</b>");
         }
         document.getElementById("evaluation3").innerHTML = cuestionThree;
 
 //pregunta4
     var question4 = prompt ("Selecciona el inciso correcto: \n4.- ¿Cuál es la dirección de Bob Esponja en Fondo de Bikini?\n\na) #125 Sirenhead Street \nb) #666 Hell Street \nc) En una piña debajo del mar \nd) #124 Conch Street")
     console.log(question4)
-    document.getElementById("recivedAnswer4").innerHTML = ("Tu respuesta: " + question4)
+    document.getElementById("recivedAnswer4").innerHTML = ("Tu respuesta: " + question4);
 
     var  correctAnswer4 = "d";
 
     if (question4 == correctAnswer4){
-        alert("Wow! Estás atento a los detalles :D")
+        alert("Wow! Estás atento a los detalles :D");
+        i++;
     } else {
-        alert("No bro :C")
+        alert("No bro :C");
     }
 
     if (question4 == correctAnswer4){
-        cuestionFour = ("<b>Correcto</b>")
+        cuestionFour = ("<b>Correcto</b>");
     }   else {
-        cuestionFour = ("Incorrecto, la respuesta correcta es:\n <b>d) #124 Conch Street</b>")
+        cuestionFour = ("Incorrecto, la respuesta correcta es:\n <b>d) #124 Conch Street</b>");
     }
     document.getElementById("evaluation4").innerHTML = cuestionFour;
 
 //pregunta5
     var question5 = prompt ("Selecciona el inciso correcto: \n5.- ¿Cuándo es la fecha de nacimiento de Bob Esponja?\n\na) 14 de julio de 1986 \nb) 25 de diciembre de 1986 \nc) 14 de febrero de 2021 \nd) 16 de septiembre de 1910")
     console.log(question5)
-    document.getElementById("recivedAnswer5").innerHTML = ("Tu respuesta: " + question5)
+    document.getElementById("recivedAnswer5").innerHTML = ("Tu respuesta: " + question5);
 
     var  correctAnswer5 = "a";
 
     if (question5 == correctAnswer5){
-        alert("Correcto! Eres bueno para recordar cumpleaños :D")
+        alert("Correcto! Eres bueno para recordar cumpleaños :D");
+        i++;
     } else {
-        alert("Nope! :(")
+        alert("Nope! :(");
     }
 
     if (question5 == correctAnswer5){
-        cuestionFive = ("<b>Correcto</b>")
+        cuestionFive = ("<b>Correcto</b>");
     }   else {
-        cuestionFive = ("Incorrecto, la respuesta correcta es:\n <b>a) 14 de julio de 1986 </b>")
+        cuestionFive = ("Incorrecto, la respuesta correcta es:\n <b>a) 14 de julio de 1986 </b>");
     }
     document.getElementById("evaluation5").innerHTML = cuestionFive;
 
@@ -118,6 +124,7 @@ if (confirm("Argh!! Bienvenido a la tripulación " + userName + " ¿Eres tan rud
 
     if (question6 == correctAnswer6){
         alert("De eso sí sabes!")
+        i++;
     } else {
         alert("Se parece, pero no")
     }
@@ -138,6 +145,7 @@ if (confirm("Argh!! Bienvenido a la tripulación " + userName + " ¿Eres tan rud
 
     if (question7 == correctAnswer7){
         alert("Ese cangrejo es un pillo!")
+        i++;
     } else {
         alert("Casi le atinas")
     }
@@ -150,7 +158,7 @@ if (confirm("Argh!! Bienvenido a la tripulación " + userName + " ¿Eres tan rud
     document.getElementById("evaluation7").innerHTML = cuestionSeven;
 
 //pregunta8
-    var question8 = prompt ("Selecciona el inciso correcto: \n8.- En el capítulo 'Semana pre-hibernación' (2001) ¿qué banda prestó una versión instrumental para armonizar el episodio?\n\na) J.Balvin \nb) Megadeth \nc) PinkFloyd \nd) Pantera")
+    var question8 = prompt ("Selecciona el inciso correcto: \n8.- En el capítulo 'Semana pre-hibernación' (2001) ¿qué banda prestó una versión instrumental para armonizar el episodio?\n\na) J. Balvin \nb) Megadeth \nc) PinkFloyd \nd) Pantera")
     console.log(question8)
     document.getElementById("recivedAnswer8").innerHTML = ("Tu respuesta: " + question8)
 
@@ -158,6 +166,7 @@ if (confirm("Argh!! Bienvenido a la tripulación " + userName + " ¿Eres tan rud
 
     if (question8 == correctAnswer8){
         alert("Se rifaron con la versión!!")
+        i++;
     } else {
         alert(":C No amigo")
     }
@@ -170,16 +179,17 @@ if (confirm("Argh!! Bienvenido a la tripulación " + userName + " ¿Eres tan rud
     document.getElementById("evaluation8").innerHTML = cuestionSeven;
 
 //pregunta9
-    var question9 = prompt ("Selecciona el inciso correcto: \n9.- ¿Qué personaje es reconocido por expresar '¡Mi pierna!'?\n\na) Larry \nb) Patricio \nc) Fred \nd) Calamardo")
+    var question9 = prompt ("Selecciona el inciso correcto: \n9.- ¿Qué personaje es reconocido por exclamar '¡Mi pierna!'?\n\na) Larry \nb) Patricio \nc) Fred \nd) Calamardo")
     console.log(question9)
     document.getElementById("recivedAnswer9").innerHTML = ("Tu respuesta: " + question9)
 
     var  correctAnswer9 = "c";
 
     if (question9 == correctAnswer9){
-        alert("Se nota que conoces a muchos personajes!!")
+        alert("Se nota que conoces a muchos personajes!!");
+        i++;
     } else {
-        alert("Oh no, esto lastimó más la pierna de Fred")
+        alert("Oh no, esto lastimó más la pierna de Fred");
     }
 
     if (question9 == correctAnswer9){
@@ -197,14 +207,15 @@ if (confirm("Argh!! Bienvenido a la tripulación " + userName + " ¿Eres tan rud
     var  correctAnswer10 = "a";
 
     if (question10 == correctAnswer10){
-        alert("Wow, gran observación!!")
+        alert("Wow, gran observación!!");
+        i++;
     } else {
         alert("Lo llegué a pensar también pero no es :C")
     }
 
     if (question10 == correctAnswer10){
         cuestionTen = ("<b>Correcto</b>")
-    }   else {
+    } else {
         cuestionTen = ("Incorrecto, la respuesta correcta es:\n <b>a) Pulpo</b>")
     }
     document.getElementById("evaluation10").innerHTML = cuestionTen;
@@ -216,4 +227,34 @@ if (confirm("Argh!! Bienvenido a la tripulación " + userName + " ¿Eres tan rud
 //document.body.style.background = "Assets/Fondo cielo.jp";
 
 //acceder a html desde javascript document
-document.getElementById('title').innerHTML = ("Bienvenido " + userName);
+document.getElementById('title').innerHTML = ("Bienvenid@ " + userName);
+
+document.getElementById("final").innerHTML = ("Estos son tus resultados: " + i + " /10");
+
+document.getElementById("score").innerHTML = (score)
+
+//Switch para determinar una evaluación con base en las respuestas correctas e incorrectas. 
+    switch (i) {
+        case 0:
+        case 1:
+        case 2:
+        document.getElementById("score").innerHTML =  "No estás listo para esta conversación  u.u";        
+            break;
+        case 3:
+        case 4:
+        document.getElementById("score").innerHTML =  "Tal vez sea estupido, pero también es tonto";
+            break;
+        case 5:
+        case 6:
+        document.getElementById("score").innerHTML =  "Te hace falta ver más Bob";
+            break;
+        case 7:
+        case 8:
+        document.getElementById("score").innerHTML =  "¡Muy bien! Los pensamientos en tu mente son un enigma";
+          break;
+        case 9:
+        case 10:
+        document.getElementById("score").innerHTML =  "¡Excelente! ¡Permitame estrechar su mano de poeta a poeta!";
+        default:
+          break;
+      }
